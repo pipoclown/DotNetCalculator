5b634413cd497ec3124604319e7fcd1dd42213af
spiraltrain-gituser <gitcourse@spiraltrain.nl>
2020-04-18 16:29:06 +0200

    Added Multiply

--- Program.cs
+++ Program.cs
@@ -11,10 +11,10 @@ namespace DotNetCalculator
     public int subtract(int i, int j) {
        return i - j;
     }
-    //public int multiply(int i, int j)
-    //{
-    //  return i * j;
-    //}
+    public int multiply(int i, int j)
+    {
+      return i * j;
+    }
     //public int divide(int i, int j)
     //{
     //  return i / j;
@@ -29,7 +29,7 @@ namespace DotNetCalculator
       Calculator c = new Calculator();
       Console.WriteLine(c.add(2, 3));
       Console.WriteLine(c.subtract(2, 3));
-      //Console.WriteLine(c.multiply(2, 3));
+      Console.WriteLine(c.multiply(2, 3));
       //Console.WriteLine(c.divide(2, 3));
     }
   }
