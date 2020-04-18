59549da5420d9fbe9ceacd51302c4b771f57fd79
spiraltrain-gituser <gitcourse@spiraltrain.nl>
2020-04-18 15:21:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DotNetCalculator
+{
+  public class Calculator
+  {
+    public int add(int i, int j)
+    {
+      return i + j;
+    }
+    //public int subtract(int i, int j)
+    //{
+    //  return i - j;
+    //}
+    //public int multiply(int i, int j)
+    //{
+    //  return i * j;
+    //}
+    //public int divide(int i, int j)
+    //{
+    //  return i / j;
+    //}
+  }
+
+  class DotNetCalculator
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello Calculator!");
+      Calculator c = new Calculator();
+      Console.WriteLine(c.add(2, 3));
+      //Console.WriteLine(c.subtract(2, 3));
+      //Console.WriteLine(c.multiply(2, 3));
+      //Console.WriteLine(c.divide(2, 3));
+    }
+  }
+}
