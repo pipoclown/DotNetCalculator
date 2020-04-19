fca4f8b7ae064c044b7f75c24d1eb33d6e42e5a4
spiraltrain-gituser <gitcourse@spiraltrain.nl>
2020-04-19 22:19:07 +0200

    Error in add

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace DotNetCalculator
   {
     public int add(int i, int j)
     {
-      return i + j;
+      return i - j;
     }
     public int subtract(int i, int j) {
        return i - j;
